7c3d050996446655c470297ac0bc126099d96138
saturnine.mh <zainnasir899@icloud.com>
2022-11-05 01:04:55 -0400

    s

--- 7 KYU /sum-even-numbers.js
+++ 7 KYU /sum-even-numbers.js
@@ -3,7 +3,7 @@
 
 // Only numbers without decimals like 4 or 4.0 can be even.
 
-// The input is a sequence of numbers: integers and/or floats.
+// The input is a sequence of numbers: integers and/or floats..
 
 // Examples
 // [4, 3, 1, 2, 5, 10, 6, 7, 9, 8]  -->  30   # because 4 + 2 + 10 + 6 + 8 = 30
