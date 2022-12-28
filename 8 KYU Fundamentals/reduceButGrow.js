b8ecfc77e1951c8f8815afce31f16d0a395dcc6b
saturnine.mh <zainnasir899@icloud.com>
2022-12-27 20:10:27 -0500

    commit

---
+++ 8 KYU Fundamentals/reduceButGrow.js
@@ -0,0 +1,15 @@
+// Reduce but Grow
+
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+function grow(x){
+    let answer = 1
+  for(let i= 0; i<x.length; i++){
+    answer *= x[i]
+    console.log(answer)
+  }
+    return answer
+  }
+  
